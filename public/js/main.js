013e1ac0fab7e27d67a66c6f49e4fcf29d400399
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2024-01-26 19:44:18 +0700

    update header styling

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,21 @@
-function displaySearch() {
+window.onresize = function() {
+    if(window.innerWidth >= 1024){
+        displaySearch('close');
+    }
+}
+
+function displaySearch(condition) {
     var header = document.getElementById("header"); 
-    var x = document.getElementById("search-container");
+    var x = document.getElementById("mobile-search-container");
     var y = document.getElementById("search-icon");
     var z = document.getElementById("close-icon");
-    if (x.style.display == "block") {
+    if (condition == "close") {
         header.style.paddingBottom = "1.5rem"
         x.style.display = "none";
         y.style.display = "block";
         z.style.display = "none";
     } else {
-        header.style.paddingBottom = "0.5rem"
+        header.style.paddingBottom = "1rem"
         x.style.display = "block";
         y.style.display = "none";
         z.style.display = "block";
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -19,19 +19,31 @@
             <div class="title">
                 <a href="/">AnimeSongList</a>
             </div>
+            <div id="search-container">
+                <div class="search-box">
+                    <form action="/">
+                        <input name="search" id="search" type="text" placeholder=" Search Anime...">
+                        <button type="submit" class="search-btn">
+                            <i class="fa-solid fa-magnifying-glass fa-xl"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+            {{-- Mobile Search icon --}}
             <div class="icon">
-                <div id="search-icon" onclick="displaySearch()">
+                <div id="search-icon" onclick="displaySearch('show')">
                     <i class="fa-solid fa-magnifying-glass fa-xl"></i>
                 </div>
-                <div id="close-icon" onclick="displaySearch()">
+                <div id="close-icon" onclick="displaySearch('close')">
                     <i class="fa-solid fa-xmark fa-xl"></i>
                 </div>
             </div>
         </div>
-        <div id="search-container">
+        {{-- Mobile Search Bar --}}
+        <div id="mobile-search-container">
             <div class="search-box">
                 <form action="/">
-                    <input name="search" id="search" type="text" placeholder="Search Anime...">
+                    <input name="search" id="search" type="text" placeholder=" Search Anime...">
                     <button type="submit" class="search-btn">
                         <i class="fa-solid fa-magnifying-glass fa-xl"></i>
                     </button>
