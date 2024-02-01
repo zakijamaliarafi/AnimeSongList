2945f447fa7782601a07266ab32e6f976b853a1e
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2024-02-01 11:43:13 +0700

    add show page

---
+++ resources/views/anime/show.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+    {{-- Bar Start --}}
+    <x-bar>
+        <div class="bar">
+            <div class="bar-text">
+                <p>{{$anime->title}}</p>
+            </div>
+        </div>
+    </x-bar>
+    {{-- Bar End --}}
+    
+    {{-- Main Content Start --}}
+    <main class="container">
+        
+    </main>
+    {{-- Main Content End --}}
+</x-layout>
