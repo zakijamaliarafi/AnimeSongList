560b38934b37b2135355df5050d6b52c664eb588
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2024-02-01 11:41:00 +0700

    add seed for anime list table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Anime;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,17 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Anime::create([
+            'title' => 'Ore dake Level Up na Ken',
+            'alt_title' => 'Solo Leveling',
+            'airing_season' => 'winter 2024'
+        ]);
+
+        Anime::create([
+            'title' => 'Youkoso Jitsuryoku Shijou Shugi no Kyoushitsu e 3rd Season',
+            'alt_title' => 'Solo Leveling',
+            'airing_season' => 'winter 2024'
+        ]);
     }
 }
