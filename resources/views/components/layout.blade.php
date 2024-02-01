c1a2ce02ffdc49bfe04ace357a05ca4840ff8ca5
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2024-02-01 11:42:57 +0700

    update href

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/components/header.css">
-    <link rel="stylesheet" href="css/components/bar.css">
-    <link rel="stylesheet" href="css/components/catalog.css">
-    <link rel="stylesheet" href="css/components/footer.css">
-    <link rel="stylesheet" href="css/utils.css">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/components/header.css')}}">
+    <link rel="stylesheet" href="{{asset('css/components/bar.css')}}">
+    <link rel="stylesheet" href="{{asset('css/components/catalog.css')}}">
+    <link rel="stylesheet" href="{{asset('css/components/footer.css')}}">
+    <link rel="stylesheet" href="{{asset('css/utils.css')}}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&display=swap" rel="stylesheet">
@@ -67,6 +67,6 @@
         </div>
     </footer>
     {{-- Footer Start --}}
-    <script src="js/main.js"></script>
+    <script src="{{asset('js/main.js')}}"></script>
 </body>
 </html>
