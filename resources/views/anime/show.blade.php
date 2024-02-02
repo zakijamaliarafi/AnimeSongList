20c40122350118eccc47b393548a135211a63dad
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2024-02-02 17:15:06 +0700

    add content section

--- resources/views/anime/show.blade.php
+++ resources/views/anime/show.blade.php
@@ -11,7 +11,32 @@
     
     {{-- Main Content Start --}}
     <main class="container">
-        
+        <div class="content">
+            <div class="content-image">
+                <img src="{{asset('images/solo.jpg')}}" alt="">
+            </div>
+            <div class="content-detail">
+                <div class="content-theme">
+                    <div id="op-theme" class="theme-title">
+                        <p>Opening Theme</p>
+                    </div>
+                    <div class="theme-song">
+                        <i class="fa-regular fa-circle-play fa-lg"></i>
+                        <p>"LEveL" by SawanoHiroyuki[nZk]:TOMORROW X TOGETHER</p>
+                    </div>
+                </div>
+                <div class="content-theme">
+                    <div class="theme-title">
+                        <p>Ending Theme</p>
+                    </div>
+                    <div class="theme-song">
+                        <i class="fa-regular fa-circle-play fa-lg"></i>
+                        <p>"request" by krage</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </main>
     {{-- Main Content End --}}
 </x-layout>
