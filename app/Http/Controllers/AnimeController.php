5f33bcf94ced7ae605d2978c402f9214394fe7f5
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2024-02-03 11:20:31 +0700

    add filter function

--- app/Http/Controllers/AnimeController.php
+++ app/Http/Controllers/AnimeController.php
@@ -9,8 +9,18 @@ class AnimeController extends Controller
 {
     // Show all anime
     public function index() {
+        // get title for bar
+        $bar_title = 'Winter 2024'; // default title
+        if(request('search') ?? false) {
+            $bar_title = 'Search Results for ' . '"' . request('search') . '"';
+        }
+        if(request('season') ?? false && request('year') ?? false) {
+            $bar_title = request('season') . ' ' . request('year');
+        }
+
         return view('anime.index', [
-            'anime_list' => Anime::all()
+            'anime_list' => Anime::filter(request(['search', 'season', 'year']))->get(),
+            'bar_title' => $bar_title
         ]);
     }
 
--- app/Models/Anime.php
+++ app/Models/Anime.php
@@ -15,4 +15,18 @@ class Anime extends Model
      * @var string
      */
     protected $table = 'anime_list';
+
+    public function scopeFilter($query, array $filters) {
+        // Filter by search
+        if($filters['search'] ?? false){
+            $query->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('alt_title', 'like', '%' . request('search') . '%');
+        } else {    // Filter by airing season
+            $airing_season = 'Winter 2024'; // Default airing season
+            if($filters['season'] ?? false && $filters['year'] ?? false){
+                $airing_season = $filters['season'] . " " . $filters['year'];
+            }
+            $query->where('airing_season', 'like', '%' . $airing_season . '%');
+        }
+    }
 }
