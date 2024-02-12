015a6d5a4241a30c0734e110f8ccc836dba8b800
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2024-02-12 18:55:14 +0700

    remove header

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,45 +18,6 @@
     <title>AnimeSongList</title>
 </head>
 <body>
-    {{-- Header Start --}}
-    <header class="container">
-        <div class="header" id="header">
-            <div class="title">
-                <a href="/">AnimeSongList</a>
-            </div>
-            <div id="search-container">
-                <div class="search-box">
-                    <form action="/">
-                        <input name="search" id="search" type="text" placeholder=" Search Anime...">
-                        <button type="submit" class="search-btn">
-                            <i class="fa-solid fa-magnifying-glass fa-xl"></i>
-                        </button>
-                    </form>
-                </div>
-            </div>
-            {{-- Mobile Search icon --}}
-            <div class="icon">
-                <div id="search-icon" onclick="displaySearch('show')">
-                    <i class="fa-solid fa-magnifying-glass fa-xl"></i>
-                </div>
-                <div id="close-icon" onclick="displaySearch('close')">
-                    <i class="fa-solid fa-xmark fa-xl"></i>
-                </div>
-            </div>
-        </div>
-        {{-- Mobile Search Bar --}}
-        <div id="mobile-search-container">
-            <div class="search-box">
-                <form action="/">
-                    <input name="search" id="search" type="text" placeholder=" Search Anime...">
-                    <button type="submit" class="search-btn">
-                        <i class="fa-solid fa-magnifying-glass fa-xl"></i>
-                    </button>
-                </form>
-            </div>
-        </div>
-    </header>
-    {{-- Header End --}}
     
     {{$slot}}
 
