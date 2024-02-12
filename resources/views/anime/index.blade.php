4834288490594e35c84d9a35b47f3ebfc4944167
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2024-02-12 18:54:51 +0700

    add header, modify bar, delete comment

--- resources/views/anime/index.blade.php
+++ resources/views/anime/index.blade.php
@@ -1,4 +1,16 @@
 <x-layout>
+    {{-- Header Start --}}
+    <header class="container">
+        <div class="header" id="header">
+            <div class="title">
+                <a href="/">AnimeSongList</a>
+            </div>
+            @include('partials._search')
+        </div>
+        @include('partials._search-mobile')
+    </header>
+    {{-- Header End --}}
+
     {{-- Bar Start --}}
     <x-bar>
         @if(str_contains($bar_title,'Search'))
@@ -12,27 +24,9 @@
                 <div class="bar-text">
                     <p>{{$bar_title}}</p>
                 </div>
-                <div class="arrow">
-                    <div class="arrow-down" id="arrow-down" onclick="displayBar('show')">
-                        <i class="fa-solid  fa-chevron-down fa-xl"></i>
-                    </div>
-                    <div class="arrow-up" id="arrow-up" onclick="displayBar('close')">
-                        <i class="fa-solid fa-chevron-up fa-xl"></i>
-                    </div>
-                </div>
+                @include('partials._arrow')
             </div>
-            <div class="bar-form" id="bar-form">
-                <form action="/">
-                    <select name="season">
-                        <option value="Winter">Winter</option>
-                        <option value="Spring">Spring</option>
-                        <option value="Summer">Summer</option>
-                        <option value="Fall">Fall</option>
-                    </select>
-                    <input name="year" type="text" placeholder=" Year" required>
-                    <button type="submit">Go</button>
-                </form>
-            </div>
+            @include('partials._select-season')
         @endif
     </x-bar>
     {{-- Bar End --}}
@@ -51,54 +45,6 @@
 
             @endif
 
-            {{-- <div class="card">
-                <div class="card-image">
-                    <a href="/"><img src="{{asset('images/solo.jpg')}}" alt="Solo Leveling"></a>
-                </div>
-                <div class="card-title">
-                    <a href=""><p>Ore dake Level Up na Ken</p></a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-image">
-                    <a href="/"><img src="{{asset('images/cote.jpg')}}" alt="Classroom of the Elite III"></a>
-                </div>
-                <div class="card-title">
-                    <a href="/"><p>Youkoso Jitsuryoku Shijou Shugi no Kyoushitsu e 3rd Season</p></a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-image">
-                    <a href="/"><img src="{{asset('images/mashle.jpg')}}" alt="Mashle"></a>
-                </div>
-                <div class="card-title">
-                    <a href="/"><p>Mashle: Shinkakusha Kouho Senbatsu Shiken-hen</p></a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-image">
-                    <a href="/"><img src="{{asset('images/tsukimichi.jpg')}}" alt="Tsukimichi"></a>
-                </div>
-                <div class="card-title">
-                    <a href="/"><p>Tsuki ga Michibiku Isekai Douchuu 2nd Season</p></a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-image">
-                    <a href="/"><img src="{{asset('images/dunmes.jpg')}}" alt="Delicious in Dungeon"></a>
-                </div>
-                <div class="card-title">
-                    <a href="/"><p>Dungeon Meshi</p></a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-image">
-                    <a href="/"><img src="{{asset('images/mato.jpg')}}" alt="Chained Soldier"></a>
-                </div>
-                <div class="card-title">
-                    <a href="/"><p>Mato Seihei no Slave</p></a>
-                </div>
-            </div> --}}
         </div>
     </main>
     {{-- Main Content End --}}
