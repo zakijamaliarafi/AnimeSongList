d314d7fb332f2e50f7383c5c208da93b47f96d32
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2024-02-17 21:04:07 +0700

    dynamic image source

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -3,7 +3,7 @@
 <div class="card">
     <a href="/anime/{{$anime->id}}">
         <div class="card-image">
-            <img src="{{asset('images/no-image.jpg')}}" alt="">
+            <img src="{{$anime->poster ? asset('storage/' . $anime->poster) : asset('/images/no-image.jpg')}}" alt="">
         </div>
         <div class="card-title">
             <p>{{$anime->title}}</p>
