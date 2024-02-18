6751786330b5da006b3f95a2095226c75e0c7953
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2024-02-18 19:19:47 +0700

    migration for songs table

---
+++ database/migrations/2024_02_18_103735_create_songs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('anime_id')->constrained(
+                table: 'anime_list', indexName: 'id'
+            );
+            $table->string('title');
+            $table->string('artist');
+            $table->string('url')->nullable();
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('songs');
+    }
+};
